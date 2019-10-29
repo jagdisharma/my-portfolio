877889f863dd0d5ba5d21787a6e78aba558a096d
Jagdish Sharma <jagdishcsharma29@gmail.com>
2019-10-29 09:15:51 +0530

    protfolio complete

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class Blog(models.Model):
     pub_data = models.DateTimeField()
     body = models.TextField(max_length=3000)
     image = models.ImageField(upload_to='blog_images/')
+
+    def __str__(self):
+        return self.title[:100]
+
+    def summary(self):
+        return self.body[:220]
